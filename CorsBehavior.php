9f621f5a89e5ee31c7db8ef25f1131cccbec8c83
Christopher Emmick <chris.emmick@blueframetech.com>
2017-07-24 13:26:42 -0400

    Created settings variable to store multiple route -> allowedOrigin comparisons.

--- CorsBehavior.php
+++ CorsBehavior.php
@@ -11,15 +11,13 @@
  * CorsBehavior Automatically adds the Access-Control-Allow-Origin response 
  * header for specific routes.
  * 
- * @version 1.0
+ * @version 2.0
  *
  */
 class CorsBehavior extends CBehavior
 {
     
-    private $_allowOrigin;
-    
-    private $_route = array();
+    private $_settings = null;
     
     
     public function events()
@@ -31,45 +29,70 @@ class CorsBehavior extends CBehavior
     
     public function onBeginRequestHandler($event)
     {
-        if (is_null($this->_allowOrigin))
+        if (is_null($this->_settings))
         {
             return;
         }
-        
-        if ($this->checkAllowedRoute())
-        {
-            
-            $origin = $this->parseHeaders();
-            
-            if ($origin !== false)
-            {
-                $this->setAllowOriginHeader($origin);
-            }
-        }
+
+	    $origin = $this->parseHeaders();
+		if ($origin !== false) {
+			foreach ($this->_settings as $setting) {
+				if ( $this->checkAllowedRoute($setting['route']) && $this->checkOrigin($origin, $setting['allowedOrigin']))
+				{
+					$this->setAllowOriginHeader($origin);
+					return;
+				}
+			}
+		}
+        // if we reach this point, we set the header to a blank value, which tells the client this origin is not allowed to access the requested route
+	    $this->setAllowOriginHeader("");
+
     }
-    
-    
+
+
+	/**
+	 * Sets a many-to-many relationship between routes and allowed origins.
+	 * @param mixed settings An unindexed array of settings in the following format
+	 *  [
+	 *      'route' => array of route(s) or the string "*" (for all routes)
+	 *      'allowedOrigin' => array of origin(s)
+	 *  ]
+	 * @throws CException
+	 */
+
+	public function setSettings($settings) {
+		if (!is_array($settings)) {
+			throw new CException('Invalid CORS settings - should be an array of settings');
+		}
+
+		foreach ($settings as $s) {
+			$this->validateRoute($s['route']);
+			$this->validateAllowedOrigin($s['allowedOrigin']);
+		}
+
+		$this->_settings = $settings;
+	}
+
     /**
-     * Sets list of routes for CORS-requests.
+     * check list of routes for CORS-requests.
      * @param mixed $route An array of routes (controllerID/actionID). If you 
      * want to allow CORS-requests for any routes, the value of the parameter
      * must be a string that contains the "*". 
      * @throws CException
      */
-    public function setRoute($route)
+    public function validateRoute($route)
     {
-        if (!is_array($route) && $route !== '*')
+		if (!is_array($route) && $route !== '*')
         {
-            throw new CException('The value of the "route" property must be an '
+            throw new CException('The value of the "route" property in settings must be an '
                     . 'array or a string that contains the "*".');
         }
-        
-        $this->_route = $route;
+
     }
     
     
     /**
-     * Sets the allowed origin.
+     * checks the allowed origin.
      * @param string $origin The origin that is allowed to access the resource.
      * A "*" can be specified to enable access to resource from any origin. 
      * A wildcard can be used to specify list of allowed origins, 
@@ -77,15 +100,18 @@ class CorsBehavior extends CBehavior
      * sub.sub.yourdomain.com will be allowed origins in that case)
      * @throws CExecption
      */
-    public function setAllowOrigin($origin)
+    public function validateAllowedOrigin($origin)
     {
-        if (!is_string($origin))
+        if (is_array($origin)) {
+        	foreach( $origin as $o) {
+        		$this->checkAllowedOrigin($o);
+	        }
+        }
+    	elseif (!is_string($origin))
         {
-            throw new CExecption('The value of the "allowOrigin" property must be '
+            throw new CExecption('The value of the "allowOrigin" property in settings must be '
                     . 'a string.');
         }
-        
-        $this->_allowOrigin = $origin;
     }
     
     
@@ -125,23 +151,7 @@ class CorsBehavior extends CBehavior
             return false;
         }
         
-        if(strlen($this->_allowOrigin) === 1)
-        {
-            return $headers['origin'];
-        }
-        
-        if (stripos($this->_allowOrigin, '*') === false)
-        {
-            return $origin === $this->_allowOrigin ? $headers['origin'] : false;
-        }
-        
-        $pattern = '/' . substr($this->_allowOrigin, 1) . '$/';
-        
-        if (substr($this->_allowOrigin, 2) === $origin
-                || preg_match($pattern, $origin) === 1)
-        {
-            return $headers['origin'];
-        }
+		else return $origin;
         
     }
     
@@ -150,22 +160,45 @@ class CorsBehavior extends CBehavior
      * Checks if CORS-request is allowed for the current route.
      * @return boolean Whether CORS-request is allowed for the current route.
      */
-    protected function checkAllowedRoute()
+    protected function checkAllowedRoute($settingRoute)
     {
-        if ($this->_route === '*')
+	    if ($settingRoute === '*')
         {
             return true;
         }
         
-        $route = Yii::app()->getUrlManager()
+        $requestedRoute = Yii::app()->getUrlManager()
                 ->parseUrl(Yii::app()->getRequest());
+
+        $wildcardRequestedRoute = preg_replace('#([^/]*)$#', '*', $requestedRoute, 1);
         
-        $wildcardRoute = preg_replace('#([^/]*)$#', '*', $route, 1);
-        
-        return in_array($route, $this->_route) || in_array($wildcardRoute, $this->_route);
+        return in_array($requestedRoute, $settingRoute) || in_array($wildcardRequestedRoute, $settingRoute);
     }
     
     
+    protected function checkOrigin ($origin, $allowedOrigins) {
+		foreach($allowedOrigins as $ao) {
+			if(strlen($ao) === 1)
+			{
+				return true;
+			}
+
+			if (stripos($ao, '*') === false && $origin === $ao)
+			{
+				return true;
+			}
+
+			$pattern = '/' . substr($ao, 1) . '$/';
+
+			if (substr($ao, 2) === $origin
+			    || preg_match($pattern, $origin) === 1)
+			{
+				return true;
+			}
+		}
+		return false;
+    }
+
     /**
      * Sets Access-Control-Allow-Origin response header.
      * @param string $origin the value of the Access-Control-Allow-Origin response 
